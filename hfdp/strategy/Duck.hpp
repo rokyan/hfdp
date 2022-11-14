95861ec6961f3a04fa0f598f9dde35cf68537a1d
Edgar Rokian <edrokyan@microsoft.com>
2022-11-15 00:05:47 +0100

    Remove merging markers.

--- hfdp/strategy/Duck.hpp
+++ hfdp/strategy/Duck.hpp
@@ -1,68 +1,33 @@
 #pragma once
 
-<<<<<<< HEAD
 #include "FlyBehavior.hpp"
 #include "QuackBehavior.hpp"
-=======
-#include "fly_behavior.hpp"
-#include "quack_behavior.hpp"
->>>>>>> 8d5d76fb2941e11b73f61de55f10901e703d3503
 
 #include <memory>
 
 namespace hfdp
 {
-<<<<<<< HEAD
-    class Duck
-    {
-    public:
-        Duck(std::unique_ptr<FlyBehavior> fly_behavior, std::unique_ptr<QuackBehavior> quack_behavior);
 
-        void performFly() const;
-        void performQuack() const;
-
-        void swim() const;
-
-        virtual void display() const = 0;
-
-        void setFlyBehavior(std::unique_ptr<FlyBehavior> fly_behavior);
-        void setQuackBehavior(std::unique_ptr<QuackBehavior> quack_behavior);
-
-        virtual ~Duck() = default;
-
-    private:
-        std::unique_ptr<FlyBehavior> fly_behavior;
-        std::unique_ptr<QuackBehavior> quack_behavior;
-    };
-}
-=======
-
-class duck
+class Duck
 {
 public:
-    duck(std::unique_ptr<fly_behavior> _fly_behavior, std::unique_ptr<quack_behavior> _quack_behavior);
+    Duck(std::unique_ptr<FlyBehavior> fly_behavior, std::unique_ptr<QuackBehavior> quack_behavior);
 
-    duck(const duck&) = delete;
-    duck& operator=(const duck&) = delete;
-
-    void perform_fly() const;
-
-    void perform_quack() const;
+    void performFly() const;
+    void performQuack() const;
 
     void swim() const;
 
     virtual void display() const = 0;
 
-    void set_fly_behavior(std::unique_ptr<fly_behavior> _fly_behavior);
+    void setFlyBehavior(std::unique_ptr<FlyBehavior> fly_behavior);
+    void setQuackBehavior(std::unique_ptr<QuackBehavior> quack_behavior);
 
-    void set_quack_behavior(std::unique_ptr<quack_behavior> _quack_behavior);
-
-    virtual ~duck() = default;
+    virtual ~Duck() = default;
 
 private:
-    std::unique_ptr<fly_behavior> _fly_behavior;
-    std::unique_ptr<quack_behavior> _quack_behavior;
+    std::unique_ptr<FlyBehavior> fly_behavior;
+    std::unique_ptr<QuackBehavior> quack_behavior;
 };
 
 } // namespace hfdp
->>>>>>> 8d5d76fb2941e11b73f61de55f10901e703d3503
